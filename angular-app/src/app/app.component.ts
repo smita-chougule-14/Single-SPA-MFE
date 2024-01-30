2af59bc9592f3f294e295b4ec5fd3454f80b682a
Smita Chougule <Smitac92@gmail.com>
2024-01-30 13:45:14 +0530

    Implementation of inter app communition within MFE

---
+++ angular-app/src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { getData, state$ } from '@app/utility';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'angular-app';
+  subscription!: Subscription;
+  listener!: () => void;
+
+  constructor(private renderer: Renderer2) {}
+  ngOnInit(): void {
+    getData('/data').then((data: any) => {
+      console.log('angular api data ', data);
+    });
+    this.subscription = state$.subscribe((data: any) => {
+      console.log('angular state data', data);
+    });
+    this.listener = this.renderer.listen(
+      'window',
+      'test',
+      ({ detail }: CustomEvent) => {
+        console.log('angular ', detail);
+      }
+    );
+    console.log('angular storage ', sessionStorage.getItem('shared'));
+    localStorage.setItem('shared', 'Angular data');
+  }
+  ngOnDestroy(): void {
+    state$.next({ name: 'Angular data' });
+    this.subscription.unsubscribe();
+    this.listener();
+  }
+}
---
+++ src/app-root-config.ts
@@ -0,0 +1,28 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine,
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
+
+function sendEvent() {
+  window.dispatchEvent(
+    new CustomEvent("test", { detail: { data: "parent data" } })
+  );
+}
+
+document.getElementById("parent").addEventListener("click", sendEvent);
